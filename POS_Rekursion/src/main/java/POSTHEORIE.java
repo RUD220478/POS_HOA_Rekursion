f5d180b141a81571da823ff799bf650ea6f21e58
Mladen Rudic <RUD220478@spengergasse.at>
2026-03-09 14:08:24 +0100

    Initial

---
+++ POS_Rekursion/src/main/java/POSTHEORIE.java
@@ -0,0 +1,42 @@
+public class POSTHEORIE {
+
+    private static boolean[][] werte = {
+            {false,false,false,true,true,true,false,false,true,true,false,false},
+            {false,true,true,true,false,true,true,true,true,false,false,false},
+            {true,false,true,true,false,false,true,true,false,false,true,true},
+            {true,false,false,false,true,true,true,false,false,true,true,true},
+            {true,true,true,false,false,false,false,true,true,true,false,false},
+            {true,true,false,true,true,false,false,true,false,false,true,true},
+            {false,false,false,true,false,false,true,false,true,true,true,false},
+            {false,false,false,false,false,true,true,true,true,false,false,false}
+            };
+
+    public static int zusammenhang(boolean[][] werte){
+        for (int i = 0; i < werte.length; i++){
+            for (int j = 0; j < werte[i].length; j++){
+                if (werte[i][j] == true){
+                    System.out.println("ja");
+                }
+                else {
+                    System.out.println("Nein. Nicht true.");
+                }
+            }
+        }
+        return -99;
+    }
+
+    public static int groesstesDing(int i, int j){
+        int n = 0;
+        int z = 0;
+        if (werte[i][j-1] == true){
+            n = j - 1;
+            z = j;
+            groesstesDing(n,j);
+        }
+        return -99;
+    }
+
+    public static void main(String[] arg){
+        zusammenhang(werte);
+    }
+}
