a681e589496635527062474eb9da2013b2c83a79
Mladen Rudic <RUD220478@spengergasse.at>
2026-03-09 20:11:25 +0100

    Funktionierendes Programm

--- POS_Rekursion/src/main/java/Rekursion.java
+++ POS_Rekursion/src/main/java/Rekursion.java
@@ -5,7 +5,7 @@ public class Rekursion {
     public static int maximumLokal = 0;
     public static int maximumTotal = 0;
 
-    // Initialisiere Array
+    // Initialize static example array
     public static boolean[][] werte = {
             {false,false,false,true,true,true,false,false,true,true,false,false},
             {false,true,true,true,false,true,true,true,true,false,false,false},
@@ -17,18 +17,19 @@ public class Rekursion {
             {false,false,false,false,false,true,true,true,true,false,false,false}
             };
 
-    // Test: Werden Werte erkannt?
+    //
     public static void zusammenhang(boolean[][] werte){
 
+        // initialize a second array to keep track of visited fields
         createSecondaryArray();
 
+        // iterate over all fields in the array
         for (int i = 0; i < werte.length; i++){
             for (int j = 0; j < werte[i].length; j++){
-
                 if (werte[i][j] && !werteCheck[i][j])
                 {
+                    // call recursive function
                     checkField(i,j);
-                    System.out.println("Ausgabe: " + i + " " + j + " MaximumLokal : " + maximumLokal);
                     if (maximumLokal > maximumTotal){
                         maximumTotal = maximumLokal;
                     }
@@ -37,27 +38,25 @@ public class Rekursion {
 
             }
         }
-        System.out.println(maximumTotal);
+        System.out.println("Die Groesse betraegt " + countMaximum());
     }
 
     public static void checkField(int x, int y) throws RekursionException{
         try {
-            boolean w = werte[x][y];
-            boolean wc = werteCheck[x][y];
             if (werte[x][y] && !werteCheck[x][y]){
                 werteCheck[x][y] = true;
                 maximumLokal += 1;
                 if (x > 0){
-                    checkField(x - 1, y); //Links --oben
+                    checkField(x - 1, y); // Check field above
                 }
                 if (x < werte.length - 1){
-                    checkField(x + 1, y); //Rechts
+                    checkField(x + 1, y); // Check field below
                 }
                 if (y > 0){
-                    checkField(x,y - 1); //Rauf
+                    checkField(x,y - 1); // Check field to the left
                 }
                 if (y < werte[0].length - 1){
-                    checkField(x,y + 1); //Runter
+                    checkField(x,y + 1); // Check field to the right
                 }
             } else {
                 werteCheck[x][y] = true;
@@ -71,7 +70,7 @@ public class Rekursion {
         return maximumTotal;
     }
 
-    // Placeholder create second array and set values to false
+    // Create second array and set all values to false
     public static void createSecondaryArray(){
         werteCheck = new boolean[werte.length][werte[0].length];
         for (int i = 0; i < werteCheck.length;i++){
@@ -81,7 +80,7 @@ public class Rekursion {
         }
     }
 
-    //yo
+    // main method
     public static void main(String[] arg){
         zusammenhang(werte);
     }
