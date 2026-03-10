dc26602b13adc49aeb1fa4f4ea9701a95002ef19
Mladen Rudic <RUD220478@spengergasse.at>
2026-03-10 16:31:35 +0100

    Refactored and cleaned up many variables, added comments

--- POS_Rekursion/src/main/java/ConnectedCoordiantesInAMatrixException.java
+++ POS_Rekursion/src/main/java/ConnectedCoordiantesInAMatrixException.java
@@ -2,6 +2,7 @@ public class ConnectedCoordiantesInAMatrixException extends RuntimeException {
     public ConnectedCoordiantesInAMatrixException(String message) {
         super(message);
     }
+    // not used
     public ConnectedCoordiantesInAMatrixException(String message, Throwable cause) {
         super(message, cause);}
 }
